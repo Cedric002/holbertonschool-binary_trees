69b0b075a6923a042c7f03630aaac0813cc5fa61
Cedric002 <cedgnleblanc@gmail.com>
2024-01-09 02:29:46 -0800

    ajout du fichier 'delete tree'

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete the binary tree
+ * @binary_tree_t: pointer
+ * @tree: pointer to the root node of the node to tree to delete
+ *
+ * Return: tree deleted
+ *
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
