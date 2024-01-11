5cd77cf0772ed2ef3e9b386224bc31ae6c9f4119
Cedric002 <cedgnleblanc@gmail.com>
2024-01-11 01:08:19 -0800

    correction du fichier tree postorder

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -16,8 +16,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	}
 
-	func(tree->n);
-
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
+
+	func(tree->n);
 }
