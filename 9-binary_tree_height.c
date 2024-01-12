30ee779aa65052defcda6b31041737a17f928a68
NidiMeliodas <nidigashan@gmail.com>
2024-01-12 03:45:20 -0800

    fixing betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,6 +16,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		left = binary_tree_height(tree->left);
 		right = binary_tree_height(tree->right);
 	}
+
 	if (left > right)
 		return (left + 1);
 	else
